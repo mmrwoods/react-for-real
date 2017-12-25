c828c847c2ccb0d0e9709dbe713fdb1082f9ef2e
Mark Woods <mwoods.online.ie@gmail.com>
2017-12-25 14:39:00 +0000

    Split code into modules

---
+++ wordcounter/src/WordCounter.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ProgressBar from './ProgressBar';
+import Counter from './Counter';
+import Editor from './Editor';
+import SaveManager from './SaveManager';
+import countWords from './countWords';
+import makeFakeRequest from './makeFakeRequest';
+
+class WordCounter extends React.Component {
+  constructor() {
+    super();
+    this.state = { text: '' };
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange(currentText) {
+    this.setState(() => ({ text: currentText }));
+  }
+
+  render() {
+    const { targetWordCount } = this.props;
+    const { text } = this.state;
+    const wordCount = countWords(text);
+    const progress = wordCount / targetWordCount;
+
+    return (
+      <form className="measure pa4 sans-serif">
+        <Editor text={text} onTextChange={this.handleTextChange} />
+        <div className="flex flex-column mt3">
+          <Counter count={wordCount} />
+          <ProgressBar completion={progress} />
+          <SaveManager saveFunction={makeFakeRequest} data={this.state} />
+        </div>
+      </form>
+    )
+  }
+}
+
+export default WordCounter;
